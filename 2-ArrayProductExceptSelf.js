130f7ab2cf5e253e7c207f65647ee0a21b09bfd2
Taha Jameel <tahajameel.co@gmail.com>
2024-11-12 00:27:48 +0500

    Second Problem Solved By Dr. Taha J. Green

---
+++ 2-ArrayProductExceptSelf.js
@@ -0,0 +1,16 @@
+const input = [1, 2, 3, 4, 5];
+const output = [];
+
+for (let i = 0; i < input.length; i++) {
+  let result = 1;
+  let clonedArray = [...input];
+  delete clonedArray[i];
+
+  clonedArray.forEach(num => {
+     result *= num;
+  });
+
+  output.push(result);
+}
+
+console.log(output);
